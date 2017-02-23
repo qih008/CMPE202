14c68e3052287cd194a35ef4847e1b3f75027271
Qing Huang <hq1992518@msn.cn>
2017-02-23 15:21:08 -0800

    Hello1.java upload

--- Personal-Project/src/main/java/test.java
+++ Personal-Project/src/main/java/test.java
@@ -1,7 +1,18 @@
+import java.io.FileInputStream;
+
 /**
  * Created by qhuang on 2/23/17.
  */
+
 public class test {
+    public static void main(String[] args) throws Exception {
+        // creates an input stream for the file to be parsed
+        FileInputStream in = new FileInputStream("Hello1.java");
 
+        // parse the file
+        CompilationUnit cu = JavaParser.parse(in);
 
+        // prints the resulting compilation unit to default system output
+        System.out.println(cu.toString());
+    }
 }
