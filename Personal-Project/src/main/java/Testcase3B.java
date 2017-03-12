7533abdf6813279e8db7a2fe7043d3295c0441ff
Qing Huang <hq1992518@msn.cn>
2017-03-11 19:33:21 -0800

    update testcase3

---
+++ Personal-Project/src/main/java/Testcase3B.java
@@ -0,0 +1,15 @@
+/**
+ * Created by qhuang on 3/11/17.
+ */
+class Testcase3B extends Testcase3A {
+
+    private String hello ;
+
+    private String getA() {
+        return hello ;
+    }
+
+    private void setA( String a ) {
+        hello = a ;
+    }
+}
