049ba4b53166929fadc180a8df042dc6208ce18b
Qing Huang <hq1992518@msn.cn>
2017-03-28 15:01:01 -0700

    update read ".java" files from path

--- Personal-Project/src/main/java/javaparser.java
+++ Personal-Project/src/main/java/javaparser.java
@@ -10,8 +10,7 @@ import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 import java.beans.ParameterDescriptor;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 
@@ -19,32 +18,44 @@ public class javaparser {
 
     public static void main(String[] args) throws Exception {
         // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream("/Users/qing/Desktop/CMPE202/Personal-Project/src/main/java/Testcase3A.java");
+        //FileInputStream in = new FileInputStream("/Users/qing/Desktop/CMPE202/Tests/uml-parser-test-1");
+        File dir = new File("/Users/qing/Desktop/CMPE202/Tests/uml-parser-test-1");
 
-        // parse the file
-        CompilationUnit cu = JavaParser.parse(in);
+        File[] files = dir.listFiles(new FilenameFilter() {
+            //@Override
+            public boolean accept(File directory, String fileName) {
+                if (fileName.endsWith(".java")) {
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        for (File f : files) {
+            // parse the file
+            CompilationUnit cu = JavaParser.parse(f);
 
 
-        // prints the changed compilation unit
-        //System.out.println(cu);
+            // prints the changed compilation unit
+            //System.out.println(cu);
 
-        new ClassVisitor().visit(cu, null);
+            new ClassVisitor().visit(cu, null);
 
-        for (TypeDeclaration typeDec : cu.getTypes()) {
-            List<BodyDeclaration> members = typeDec.getMembers();
-            if (members != null) {
-                for (BodyDeclaration member : members) {
-                    //Check just members that are FieldDeclarations
-                    FieldDeclaration field = (FieldDeclaration) member;
-                    //Print the field's class type
-                    System.out.println(field.getModifiers());
-                    //Print the field's name
-                    System.out.println(field.getVariables().get(0).getType()+" "+field.getVariables().get(0).getName());
+            for (TypeDeclaration typeDec : cu.getTypes()) {
+                List<BodyDeclaration> members = typeDec.getMembers();
+                if (members != null) {
+                    for (BodyDeclaration member : members) {
+                        //Check just members that are FieldDeclarations
+                        FieldDeclaration field = (FieldDeclaration) member;
+                        //Print the field's class type
+                        System.out.println(field.getModifiers());
+                        //Print the field's name
+                        System.out.println(field.getVariables().get(0).getType() + " " + field.getVariables().get(0).getName());
+                    }
                 }
             }
         }
     }
-
     /*
             * Simple visitor implementation for visiting MethodDeclaration nodes.
      */
