cd5f9269792bc10a725f48d519d6d9022df276b1
Qing Huang <hq1992518@msn.cn>
2017-04-16 23:13:51 -0700

    Remove all temp files

--- Personal-Project/temp_test/Hello1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by hq199_000 on 2017/2/23.
- */
-public class Hello1 {
-    private String str ;
-    public String getStr() { return this.str ; }
-    public void   setStr(String value) { this.str = value ; }
-}
